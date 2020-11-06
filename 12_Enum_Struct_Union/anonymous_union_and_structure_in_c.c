03a160482484fac4b8b4dce8860d436645526d3a
Aegis Liang <algorithm_learning@163.com>
2020-11-06 08:58:30 +0800

    finish 12

---
+++ 12_Enum_Struct_Union/anonymous_union_and_structure_in_c.c
@@ -0,0 +1,25 @@
+// C Program to demonstrate working of anonymous struct
+#include<stdio.h>
+struct Scope
+{
+	// Anonymous structure
+	struct	// could be union
+	{
+		char alpha;
+		int num;
+	};
+};
+
+int main()
+{
+	struct Scope x;
+	x.num = 65;
+	x.alpha = 'B';
+
+	// Note that members of structure are accessed directly
+	printf("x.alpha = %c, x.num = %d", x.alpha, x.num);
+
+	return 0;
+}
+
+
